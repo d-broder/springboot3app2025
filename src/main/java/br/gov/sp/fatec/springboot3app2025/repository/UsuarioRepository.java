d2b09ada8c929951a172449a448699834cceb6c7
d-broder <daniel_sendreti@hotmail.com>
2025-09-04 11:00:48 -0300

    feat: Implement user entity, repository, and service for user management

---
+++ src/main/java/br/gov/sp/fatec/springboot3app2025/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.gov.sp.fatec.springboot3app2025.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.gov.sp.fatec.springboot3app2025.entity.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
